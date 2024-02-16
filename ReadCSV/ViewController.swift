05319a719ebbbe5ce8c27a40c75b5f1dd56d155e
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-16 23:33:42 +0900

    変更

--- ReadCSV/ViewController.swift
+++ ReadCSV/ViewController.swift
@@ -10,10 +10,11 @@ import SnapKit
 
 class ViewController: UIViewController {
     var csvContents: [String] = []
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        csvTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        csvTable.dataSource = self
+        csvTable.delegate = self
         view.addSubview(csvTable)
         csvTable.snp.makeConstraints { c in
             c.centerX.equalToSuperview()
@@ -22,9 +23,7 @@ class ViewController: UIViewController {
             c.bottom.equalTo(0)
             
         }
-        csvTable.register(csvTableCellTableViewCell.self, forCellReuseIdentifier: "cell")
-        csvTable.dataSource = self
-        csvTable.delegate = self
+
         guard let path = Bundle.main.path(forResource: "seichi", ofType: "csv") else {
             print("データソース不足")
             return
@@ -32,7 +31,9 @@ class ViewController: UIViewController {
         do {
              let csvString = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
              csvContents = csvString.components(separatedBy: .newlines)
+            print(csvContents)
         } catch {
+             print("無理がある")
             return
         }
 
