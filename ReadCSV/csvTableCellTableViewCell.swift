22dbd94fd032a308fca6a6c059b70c7bc3a2cd8e
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-16 22:45:45 +0900

    tablecell追加

---
+++ ReadCSV/csvTableCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  csvTableCellTableViewCell.swift
+//  ReadCSV
+//
+//  Created by 澤木柊斗 on 2024/02/16.
+//
+
+import UIKit
+
+class csvTableCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
